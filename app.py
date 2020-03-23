0c02d4422b14159554180b6f539c48c0e2eb4731
Sijun Liu <sijunliu@google.com>
2020-03-22 23:18:26 -0700

    add code

---
+++ app.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+import requests
+import google.auth.transport.requests
+import google.auth.compute_engine
+
+app = Flask(__name__)
+
+REQUESTS_SESSION = requests.Session()
+REQUESTS_SESSION.verify = False
+http_request = google.auth.transport.requests.Request(REQUESTS_SESSION)
+
+
+@app.route("/")
+def hello_world():
+    credentials = google.auth.compute_engine.IDTokenCredentials(
+        http_request, "target_audience", use_metadata_identity_endpoint=True
+    )
+    try:
+        credentials.refresh(http_request)
+    except Exception as e:
+        return str(e)
+    return credentials.token
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
